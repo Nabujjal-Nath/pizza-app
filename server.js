ffaf43b95ed343d46be0a45f621b30349da68723
nabujjal <nabujjal@gmail.com>
2020-11-04 17:29:20 +0530

    cart and auth ready

--- server.js
+++ server.js
@@ -6,15 +6,25 @@ const app=express();
 const PORT= process.env.PORT || 3300;
 //assets
 app.use(express.static('public'));
-app.get('/',(req,res)=>{
-      res.render('home.ejs');
-})
+
 //set template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs');
 
 
+app.get('/',(req,res)=>{
+    res.render('home.ejs');
+})
+app.get('/cart',(req,res)=>{
+      res.render('customers/cart.ejs');
+})
+app.get('/login',(req,res)=>{
+    res.render('auth/login');
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register');
+})
 
 app.listen(PORT,()=>{
     console.log(`Listening to port ${PORT}.`);
